929b8da2ce8ee0edeeea7ed34ec080b141aaaff4
jagajaga <senia00@gmail.com>
2013-03-15 15:32:00 +0400

    Added no input checking

--- task1/myprint.asm
+++ task1/myprint.asm
@@ -5,19 +5,28 @@ section .text
 global main
 
 main:
-	mov ecx, [esp + 4]
-	mov ebx, [esp + 8]
-	mov ebx, [ebx + 4]
-	mov si, answer
-	mov [si], ebx
-	mov ebx, [si]
-	push ebx 
+	mov ecx, [esp + 4] ;argc
+	mov ebx, [esp + 8] ;argv
+	mov ebx, [ebx + 4] ;argv[1]
+	test ebx, ebx
+		jz noinput
+	push dword[ebx]
+	push formatc
+	jmp print
+noinput:
+	push noinputstring
+	push formats
+print:
 	call printf
-	add esp, 4
+	add esp, 8
 	
 
+section .rodata
+    formatc: db "%c", 0
+    formats: db "%s", 0
+	noinputstring: db "No input", 10, 0
 section .data
-    format: db "%d", 0
 	answer: times ANSWER_LENGTH db 0
+	answerend: db 0
 
 end
