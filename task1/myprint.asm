460dbc9bd2f71285015ef894343d770290d7c922
jagajaga <senia00@gmail.com>
2013-03-16 23:40:48 +0400

    added arguments preparsing

--- task1/myprint.asm
+++ task1/myprint.asm
@@ -5,21 +5,58 @@ section .text
 global main
 
 main:
-	mov ecx, [esp + 4] ;argc
-	mov ebx, [esp + 8] ;argv
-	mov ebx, [ebx + 4] ;argv[1]
+	mov edi, [esp + 8] ;argc[1]
 	test ebx, ebx
 		jz noinput
-	push dword[ebx]
-	push formatc
-	jmp print
+	xor bh, bh
+	jmp get_flags
+
+;bh = space plus minus zero negative
+add_space:
+	or bh, 10000b
+add_plus:
+	or bh, 1000b
+add_minus:
+	or bh, 100b
+add_zero:
+	or bh, 10b
+
+get_flags:
+	mov al, [edi]
+	test al, al
+		jz get_length
+	cmp al, ' '
+		je add_space
+	cmp al, '+'
+		je add_plus
+	cmp al, '-'
+		je add_minus
+	cmp al, '0'
+		je add_zero
+	inc edi
+	jmp get_flags
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 noinput:
 	push noinputstring
 	push formats
-print:
 	call printf
 	add esp, 8
+	call exit
 	
+exit:
 
 section .rodata
     formatc: db "%c", 0
