bfc7c2328404a592743da3ba23891e2a99d75a87
jagajaga <senia00@gmail.com>
2013-03-25 22:11:50 +0400

    Working arctg version

--- row.asm
+++ row.asm
@@ -5,22 +5,60 @@ global main
 
 
 main:
+	fld qword[number]
+	fld1
+	fpatan
+	call myprint
+	mov ecx, 60000000
 	jmp arctg
 
-
-
-arctg:
+arctg:	
 	fld qword[number]
-	fsqrt
+	fld qword[mone]
+	fld qword[number]
+	fld qword[two]
+	fld qword[number]
+	fld1
+	fld qword[number]
+
+aloop:
+	fxch st0, st1
+		fadd st0, st3
+	fxch st0, st1
+	fxch st0, st2
+		fmul st0, st4
+		fmul st0, st4
+		fmul st0, st5
+	fxch st0, st2
+	fld st2
+	fdiv st0, st2
+	fxch st0, st7
+	fstp st0	
+	fadd st0, st6
+		
+	dec ecx
+	jnz aloop
+
+myprint:
 	sub esp, 8
 	fstp qword[esp]
 	push format
 	call printf
 	add esp, 12
+	call print_line
+	ret
+
+
+print_line:
+	push new_line
+	call printf
+	add esp, 4
 	ret
 	
 
 section .data
-	number: dq 0.11
-	format: db "%lf"
-
+	number: dq 0.9999
+	format: db "%.20lf"
+	new_line: db 10, 0
+	two: dq 2.0
+	mone: dq -1.0
